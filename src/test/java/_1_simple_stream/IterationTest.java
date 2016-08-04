6fd674528d4a38d95d61c618e84be3e08403248a
c.piotre <c.piotre@gmail.com>
2016-08-05 01:29:04 +0200

    Include guava in project

--- src/test/java/_1_simple_stream/IterationTest.java
+++ src/test/java/_1_simple_stream/IterationTest.java
@@ -1,17 +1,23 @@
 package _1_simple_stream;
 
+import com.google.common.base.Stopwatch;
+
+import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
 
 public class IterationTest {
     // -XX:+UnlockDiagnosticVMOptions -XX:+UnlockCommercialFeatures -XX:+FlightRecorder
     public static void main(String[] args) throws Exception {
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "128");
         System.in.read();
 
+        Stopwatch watch = Stopwatch.createStarted();
         LongStream.range(0, 1_000_000_000_000L)
                 .parallel()
                 .forEach(i -> {
                     // no-op
                 });
+        watch.stop();
+        System.out.format("%d seconds%n", watch.elapsed(TimeUnit.SECONDS));
+
     }
 }
